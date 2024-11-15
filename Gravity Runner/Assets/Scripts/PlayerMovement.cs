860748daf31cbb096fc64dd104dd54c144021235
Almerosaurus <almerzainfarisseno@gmail.com>
2024-11-16 06:01:41 +0700

    PlayerMovement Update 1

--- Gravity Runner/Assets/Scripts/PlayerMovement.cs
+++ Gravity Runner/Assets/Scripts/PlayerMovement.cs
@@ -1,12 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
-     [SerializeField] private float speed = 3f;
-    [SerializeField] private float accelaration = 0.5f;
-    [SerializeField] private float accelaration_time = 3f;
+    private Rigidbody2D myBody;
+
+    private Button jumpButton;
+
+
+    [SerializeField] private float speed = 3f;
+    [SerializeField] private float acceleration = 0.5f;
+    [SerializeField] private float acceleration_time = 3f;
+
+    void Awake() {
+        jumpButton = GameObject.Find("Jump Button").GetComponent<Button>();             // Nama tombol buat karakter loncat = Jump Button
+        jumpButton.onClick.AddListener (() => Jump ());
+        myBody = GetComponent<Rigidbody2D>();
+    }
+
 
     void Start() {
         StartCoroutine(Nambah_Kecepatan());
@@ -20,10 +33,17 @@ public class PlayerMovement : MonoBehaviour
         transform.position = temp;
     }
 
+    private void Jump() {
+        myBody.gravityScale *= -1;
+        Vector3 temp = transform.localScale;
+        temp.y *= -1;
+        transform.localScale = temp;
+    }
+
     IEnumerator Nambah_Kecepatan() {
         while (speed < 10f) {
-        yield return new WaitForSeconds(accelaration_time);
-        speed += accelaration;
+        yield return new WaitForSeconds(acceleration_time);
+        speed += acceleration;
         }
     }
 }
